66cfa46c0ff3acbe9a7e6c2315178978f038292d
Advel0 <0advel0@gmail.com>
2022-11-22 11:24:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link, useEffect} from 'react';
+import {useState} from 'react';
+
+
 
 function App() {
+  
+  const [identityInterface, setIdentityInterface] = useState([{'element': <MyButton key='connect-button' id='connect-button' button_text='Connect' func={connect} />}])
+
+
+
+  function connect(){
+    
+
+    let list = [
+      {
+        'type' : 'address',
+        'content' : '0x68b3c4c90AD57B48479F1358DeB3949A61F58a94'
+      }
+    ]
+
+    let components = list.map(component=>{
+      return {'element' : <IdentityComponent component_type={component.type} component_content={component.content} key={component.type}/>}
+    })
+    
+    // console.log(components)
+
+    setIdentityInterface(components)
+
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=' container flex flex-row pt-6 pb-6' id='page-content'>
+      <div className='basis-3/4'>
+        <Menu/>
+      </div>
+      <Identity elements={identityInterface}/>
     </div>
   );
+}
+
+function Identity({elements}){
+  return(
+    <div className='container basis-1/4 mr-6 flex flex-row justify-around' id='identity-holder'>
+      <div className='flex flex-col w-4/5' >
+        {
+          elements.map(element =>{
+            return element['element']
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+function Menu(){
+  return(
+    <div className='menu container flex flex-row mx-auto pt-3 pb-3'>
+      <div className='menu-button ml-3'>
+        <a href='/home'>HOME</a>
+      </div>
+      <div className='menu-button'>
+        <a href='/voting-creation'>CREATE A VOTING</a>
+      </div>
+      
+    </div>
+  )
+}
+
+function test(){
+  return (
+    <div key='1'>
+      test
+    </div>
+  )
+}
+
+function MyButton(props){
+  return(
+    <button id={props.id} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={props.func}>
+      {props.button_text}
+    </button>
+  )
+}
+
+function IdentityComponent(props){
+  return(
+    <span className='container identity-component'>
+      {props.component_type} | {props.component_content}
+    </span>
+  )
 }
 
 export default App;
